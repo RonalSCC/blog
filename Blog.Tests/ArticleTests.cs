0d9bf1f763ed44b6a7c3b744a7c6895913811d27
ronal.castano <ronal.castano@sinco.com.co>
2025-10-17 16:39:14 -0500

    Test para que un articulo no tenga más de 20 bloques de contenido.

--- Blog.Tests/ArticleTests.cs
+++ Blog.Tests/ArticleTests.cs
@@ -24,11 +24,26 @@ public class ArticleTests : CommandHandlerTest<InitArticle>
 
         caller.Should().ThrowExactly<InitArticleException>().WithMessage(Article.DEBE_CONTENER_AL_MENOS_UN_BLOQUE);
     }
+    
+    [Fact]
+    public void Si_CreoUnArticuloConMasDe20Bloques_Debe_GenerarUnaExcepcion()
+    {
+        var bloques = new List<object>();
+        for (int i = 0; i < 21; i++)
+        {
+            bloques.Add(new object());
+        }
+
+        var caller = () => When(new InitArticle("Articulo de testing", bloques));
+
+        caller.Should().ThrowExactly<InitArticleException>().WithMessage(Article.NO_PUEDE_TENER_MAS_DE_20_BLOQUES);
+    }
 }
 
 public class Article
 {
-    public static string DEBE_CONTENER_AL_MENOS_UN_BLOQUE = "No se puede crear un articulo sin bloques.";
+    public const string NO_PUEDE_TENER_MAS_DE_20_BLOQUES = "El articulo no puede contener más de 20 bloques";
+    public const string DEBE_CONTENER_AL_MENOS_UN_BLOQUE = "No se puede crear un articulo sin bloques.";
     public const string EL_TITULO_NO_PUEDE_SER_VACIO = "No se puede crear un articulo con el titulo vacío.";
 }
 
@@ -38,10 +53,19 @@ public class InitArticleCommandHandler(IEventStore eventStore) : ICommandHandler
 {
     public void Handle(InitArticle command)
     {
-        if (string.IsNullOrEmpty(command.Title))
+        if (IsAInvalidTitle(command))
             throw new InitArticleException(Article.EL_TITULO_NO_PUEDE_SER_VACIO);
 
-        throw new InitArticleException(Article.DEBE_CONTENER_AL_MENOS_UN_BLOQUE);
+        if(command.Block.Count == 0)
+            throw new InitArticleException(Article.DEBE_CONTENER_AL_MENOS_UN_BLOQUE);
+        
+        throw new InitArticleException(Article.NO_PUEDE_TENER_MAS_DE_20_BLOQUES);
+        
+    }
+
+    private static bool IsAInvalidTitle(InitArticle command)
+    {
+        return string.IsNullOrEmpty(command.Title);
     }
 }
 
